195e166ccfd551e00c6b5c360d7cafb245488f07
Ana Stancic <anet.persa@gmail.com>
2026-08-16 16:35:58 +0200

    question added

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,4 @@
 import { Component, signal } from '@angular/core';
-import axios from "axios";
-import { QuestionService } from '../../services/main.service';
-import { QuestionModel } from '../../models/question.model';
-import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -10,13 +6,4 @@ import { NgFor, NgIf } from '@angular/common';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-  protected webData = signal<QuestionModel[]>([])
-  protected webError = signal<any>(null)
-  constructor() {
-    QuestionService.getAllAdmissionQuestions()
-      .then(rsp => this.webData.set(rsp.data))
-      .catch(e=>this.webError.set(e))
-  }
-}
-
+export class Home { }
---
+++ src/app/question/question.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { QuestionModel } from '../../models/question.model';
+import { QuestionService } from '../../services/main.service';
+
+@Component({
+  selector: 'app-question',
+  imports: [],
+  templateUrl: './question.html',
+  styleUrl: './question.css',
+})
+export class Question {
+  protected webData = signal<QuestionModel[]>([])
+  protected webError = signal<any>(null)
+  constructor() {
+    QuestionService.getAllAdmissionQuestions()
+      .then(rsp => this.webData.set(rsp.data))
+      .catch(e => this.webError.set(e))
+  }
+}
