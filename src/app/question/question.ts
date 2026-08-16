b378497061396c1dda0daa56d7203682d31c66dd
Ana Stancic <anet.persa@gmail.com>
2026-08-16 17:12:36 +0200

    search added

--- src/app/question/question.ts
+++ src/app/question/question.ts
@@ -2,23 +2,37 @@ import { Component, signal } from '@angular/core';
 import { QuestionModel } from '../../models/question.model';
 import { QuestionService } from '../../services/main.service';
 import linkifyHtml from "linkify-html"
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './question.html',
   styleUrl: './question.css',
 })
 export class Question {
   protected webData = signal<QuestionModel[]>([])
   protected webError = signal<any>(null)
+  protected search: string = ''
+  protected previousSearch: string = 'N/A'
+
+
   constructor() {
-    QuestionService.getAllAdmissionQuestions()
-      .then(rsp => this.webData.set(rsp.data))
-      .catch(e => this.webError.set(e))
+    this.loadQuestions()
   }
 
-  convertToLinks(text : string) {
+  loadQuestions() {
+    if (this.previousSearch == '' && this.search == '') return
+    this.previousSearch = this.search
+    QuestionService.getAllAdmissionQuestions(this.search)
+      .then(rsp => this.webData.set(rsp.data))
+      .catch(e => this.webError.set(e))
+
+  }
+
+  convertToLinks(text: string) {
     return linkifyHtml(text)
   }
+
+
 }
--- src/services/main.service.ts
+++ src/services/main.service.ts
@@ -11,8 +11,8 @@ const client = axios.create({
     }
 })
 export class QuestionService {
-    static async getAllAdmissionQuestions() {
-        return await client.get<QuestionModel[]>('/category/upis')
+    static async getAllAdmissionQuestions(s : string = '') {
+        return await client.get<QuestionModel[]>(`/category/upis?search=${s}`)
 
     }
 }
