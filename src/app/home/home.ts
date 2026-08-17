43e7d81730c514bda5ca8980de151384aafd1a1d
Ana Stancic <anet.persa@gmail.com>
2026-08-17 23:01:37 +0200

    flight service added, home changed

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,33 @@
 import { Component, signal } from '@angular/core';
+import { FlightService } from '../../services/flight.service';
+import { RouterLink } from '@angular/router';
+import { FlightModel } from '../../models/flight.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home { }
+export class Home {
+  protected flights = signal<FlightModel[]>([])
+
+  constructor() {
+    FlightService.getFutureFlights()
+      .then(rsp => this.flights.set(rsp.data))
+  }
+
+  protected formatDate(iso: string) {
+    return new Date(iso).toLocaleString('sr-RS', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    })
+  }
+
+  protected getImage(dest: string) {
+    return `https://img.pequla.com/destination/${dest.split(' ')[0].toLowerCase()}.jpg`
+  }
+}
